ddac65b78db280855c1299c1c9a7d34ce32f8104
* <user@aol.com>
2018-02-24 06:02:25 +0500

    Initial release

---
+++ endelta/endelta.cpp
@@ -0,0 +1,160 @@
+/* ******************************************************************** **
+** @@ Delta
+** @  Copyrt :
+** @  Author :
+** @  Modify :
+** @  Update :
+** @  Note   :
+** ******************************************************************** */
+
+/* ******************************************************************** **
+**                uses pre-compiled headers
+** ******************************************************************** */
+
+#include "stdafx.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+#ifdef NDEBUG
+#pragma optimize("gsy",on)
+#pragma comment(linker,"/FILEALIGN:512 /MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR /IGNORE:4078")
+#endif
+
+/* ******************************************************************** **
+** @@                   internal defines
+** ******************************************************************** */
+
+/* ******************************************************************** **
+** @@                   internal prototypes
+** ******************************************************************** */
+
+/* ******************************************************************** **
+** @@                   external global variables
+** ******************************************************************** */
+
+/* ******************************************************************** **
+** @@                   static global variables
+** ******************************************************************** */
+
+/* ******************************************************************** **
+** @@                   real code
+** ******************************************************************** */
+
+/* ******************************************************************** **
+** @@ Proceed()
+** @  Copyrt :
+** @  Author :
+** @  Modify :
+** @  Update :
+** @  Notes  :
+** ******************************************************************** */
+
+static void  Proceed(const char* const pszFileName)
+{
+   char     pszDstName[_MAX_PATH];
+   char     pszDrive  [_MAX_DRIVE];
+   char     pszDir    [_MAX_DIR];
+   char     pszFName  [_MAX_FNAME];
+   char     pszExt    [_MAX_EXT];
+
+   _splitpath(pszFileName,pszDrive,pszDir,pszFName,pszExt);
+   _makepath( pszDstName,pszDrive,pszDir,pszFName,"end");
+
+   FILE*    pIn = fopen(pszFileName,"rb");
+
+   if (!pIn)
+   {
+      perror("\a\nOpen Input File Error !\n");
+      return;
+   }
+
+   FILE*    pOut = fopen(pszDstName,"wb");
+
+   if (!pOut)
+   {
+      perror("\a\nOpen Output File Error !\n");
+      fclose(pIn);
+      pIn = NULL;
+      return;
+   }
+
+   int   iPrev = fgetc(pIn);
+
+   if (iPrev == EOF)
+   {
+      return;
+   }
+
+   fputc(iPrev,pOut);
+
+   int   iNext = 0;
+
+   while ((iNext = fgetc(pIn)) != EOF)
+   {
+      fputc(iNext - iPrev,pOut);
+      iPrev = iNext;
+   }
+
+   fclose(pIn);
+   pIn = NULL;
+
+   fclose(pOut);
+   pOut = NULL;
+}
+
+/* ******************************************************************** **
+** @@ ShowHelp()
+** @  Copyrt :
+** @  Author :
+** @  Modify :
+** @  Update :
+** @  Notes  :
+** ******************************************************************** */
+
+void ShowHelp()
+{
+   const char  pszCopyright[] = "-*-  EnDelta  *  (c)  Gazlan,  2018  -*-";
+   const char  pszDescript [] = "Use Delta modulation";
+   const char  pszE_Mail   [] = "complains_n_suggestions direct to gazlan@yandex.ru";
+
+   printf("%s\n\n",pszCopyright);
+   printf("%s\n\n",pszDescript);
+   printf("Usage: endelta.com victim\n\n");
+   printf("%s\n",pszE_Mail);
+}
+
+/* ******************************************************************** **
+** @@ main()
+** @  Copyrt :
+** @  Author :
+** @  Modify :
+** @  Update :
+** @  Notes  :
+** ******************************************************************** */
+
+int main(int argc,char** argv)
+{
+   if (argc < 2)
+   {
+      ShowHelp();
+      return 0;
+   }
+
+   if (argc == 2 && ((!strcmp(argv[1],"?")) || (!strcmp(argv[1],"/?")) || (!strcmp(argv[1],"-?")) || (!stricmp(argv[1],"/h")) || (!stricmp(argv[1],"-h"))))
+   {
+      ShowHelp();
+      return 0;
+   }
+
+   Proceed(argv[1]);
+
+   return 0;
+}
+
+/* ******************************************************************** **
+**                That's All
+** ******************************************************************** */
